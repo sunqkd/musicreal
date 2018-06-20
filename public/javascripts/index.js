931796035d5235fd899c455a58dd484a2baa7b1f
sunqkd <sunzhaoqkd@163.com>
2018-06-20 23:52:58 +0800

    'update'

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,7 @@ function $(s) {
 }
 
 var lis = $("#list li");
-var size = 128
+var size = 64
 var box = $("#box")[0];
 var height, width;
 var Dots = []; // 定义随即色
@@ -33,25 +33,25 @@ for (var i = 0; i < lis.length; i++) {
 
 }
 
-
-
 function random(m, n) {
     return Math.round(Math.random() * (n - m) + m)
 }
+
 function getDots() {
     Dots = [];
     for (var i = 0; i < size; i++) {
         var x = random(0, width);
         var y = random(0, height);
-        var color = "rgb(" + random(0, 255) + " , " + random(0, 255) + "," + random(0, 255) + ")";
+        var color = "rgba(" + random(0, 255) + " , " + random(0, 255) + "," + random(0, 255) + ", 0)";
         Dots.push({
             x: x,
             y: y,
-            color: color
+            dx: random(1,3),
+            color: color,
+            cap: 0 // 小帽距离底端的距离默认为零
         })
     }
 }
-
 
 function resize() {
     height = box.clientHeight;
@@ -63,7 +63,6 @@ function resize() {
     line.addColorStop(0.5, "yellow"); // 渐变色
     line.addColorStop(1, "green"); // 渐变色
     // context.fillStyle = line;
-
     getDots()
 }
 
@@ -74,22 +73,34 @@ window.onresize = resize
 function draw(arr) {
     context.clearRect(0, 0, width, height);
     var w = width / size
+    var cw = w * 0.8 // 小帽宽度
+    var capH = cw > 8 ? 8 : cw// 小帽的高度
     context.fillStyle = line;
     for (var i = 0; i < size; i++) {
-
+        var o = Dots[i]
         if (draw.type == "column") {
             var h = arr[i] / 256 * height
             context.fillRect(w * i, height - h, w * 0.8, h)
+            // 绘制小帽
+            context.fillRect(w * i, height - (o.cap + capH), w * 0.8, capH)
+            o.cap--;
+            if(o.cap < 0){
+                o.cap = 0
+            }
+            if( h > 0 && o.cap < h + 10){
+                o.cap = h + 10 > height - capH ? height - capH : h + 10
+            }
         } else {
             context.beginPath()
-            var o = Dots[i]
-            var r = arr[i] / 256 * 50;
+            var r = 10 + arr[i] / 256 * (height > width ? width: height) /10;
             context.arc(o.x, o.y, r, 0, 2 * Math.PI)
             var g = context.createRadialGradient(o.x, o.y, 0, o.x, o.y, r)
             g.addColorStop(0, "#fff");
             g.addColorStop(1, o.color);
             context.fillStyle = g;
             context.fill()
+            o.x += o.dx
+            o.x = o.x > width ? 0 : o.x
             // context.strokeStyle = "white"
             // context.stroke()
         }
